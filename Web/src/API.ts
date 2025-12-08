64defd831596effb3d8ce54308613ab691b658ce
Greg Whatley <me@gregwhatley.dev>
2025-12-07 20:54:39 -0600

    migrate to vite

--- Web/src/API.ts
+++ Web/src/API.ts
@@ -1,5 +1,6 @@
 export function getWeatherConditions(coords: GeolocationCoordinates): Promise<{ tempC: number, location: { name: string, country: string } }> {
-    return window.fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
+    const key = (import.meta as unknown as any).env.VITE_WEATHER_API_KEY
+    return window.fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${key}&units=metric`)
         .then(r => r.json())
         .then(json => ({tempC: json.main.temp, location: {name: json.name, country: json.sys.country}}))
 }
